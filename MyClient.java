fac00b23a7723e6156f3fb0387813bc456a58bc2
Michael-Kneale <michael.kneale@students.mq.edu.au>
2022-03-17 19:43:08 +1100

    Made start on making MyClient.java more in line with user guide

--- MyClient.java
+++ MyClient.java
@@ -3,19 +3,21 @@ import java.net.*;
 public class MyClient {
 public static void main(String[] args) {
 	try {
-		Socket s = new Socket("localhost",6666);
+		Socket s = new Socket("localhost", 50000);
 		DataInputStream dis = new DataInputStream(s.getInputStream());
 		DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 		
 		dout.writeUTF("HELO");
 		
 		String str = (String)dis.readUTF();
-		dout.writeUTF("BYE");
+		dout.writeUTF("AUTH JohnUser");
 		
 		str = (String)dis.readUTF();
+		dout.writeUTF("REDY");
 		
+		//...etc.
 		
-		
+		dout.writeUTF("QUIT");		
 		dout.flush();
 		dout.close();
 		s.close();
